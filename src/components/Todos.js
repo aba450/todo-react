dfd379bd0b850b79678a07bc291d1957a7d80dd0
aba450 <aba450@nyu.edu>
2020-07-10 17:50:32 -0400

    first commit

---
+++ src/components/Todos.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import TodoItem from "./TodoItem";
+import PropTypes from "prop-types";
+
+class Todos extends Component {
+  render() {
+    console.log(this.props.todos);
+    return this.props.todos.map((todo) => (
+      <TodoItem
+        key={todo.id}
+        todo={todo}
+        markComplete={this.props.markComplete}
+        delete={this.props.delete}
+      />
+    ));
+  }
+}
+Todos.propTypes = {
+  todos: PropTypes.array.isRequired,
+  markComplete: PropTypes.func.isRequired,
+  delete: PropTypes.func.isRequired,
+};
+export default Todos;
